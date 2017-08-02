5ed7def5e2648b147570ff20e0af6cebb07adec7
zaqo <zaqo@narod.ru>
2017-08-02 22:46:28 +0300

    Checked everything in flight_by_day

--- flights_by_day.php
+++ flights_by_day.php
@@ -53,7 +53,7 @@
 			$fl_id=$row[0];
 			$nav_id=$row[1];
 			$content.= "<tr>";
-			$content.= "<td><input type=\"checkbox\" name=\"to_export[]\" class=\"flights\" value=\"$fl_id\" /></td>";
+			$content.= "<td><input type=\"checkbox\" name=\"to_export[]\" class=\"flights\" value=\"$fl_id\" checked/></td>";//
 			$content.= "<td><a href=\"check_services_mysql.php?id=$nav_id\">$num</a></td>";
 				
 			foreach ($row as $value)
